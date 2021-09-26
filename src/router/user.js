f1a7f8e2f114f400185839c8a6bbba30e3840803
nour-gamal <nmg181963@hotmail.com>
2021-09-27 00:38:12 +0200

    sending hii

--- src/router/user.js
+++ src/router/user.js
@@ -4,7 +4,10 @@ const User = require("../models/User");
 const userRouter = new express.Router();
 
 userRouter.post("/register", (req, res) => {
-	res.send(req.body);
+	res.send({
+		message: "Say hii Nour",
+		data: req.body,
+	});
 });
 
 module.exports = userRouter;
