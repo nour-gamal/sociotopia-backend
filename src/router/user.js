faa7226a14e19a66f859115c3fc2282b152e5678
nour-gamal <nmg181963@hotmail.com>
2021-09-27 00:33:44 +0200

    test 1

--- src/router/user.js
+++ src/router/user.js
@@ -4,7 +4,7 @@ const User = require("../models/User");
 const userRouter = new express.Router();
 
 userRouter.post("/register", (req, res) => {
-	res.send("hello");
+	res.send(req.body);
 });
 
 module.exports = userRouter;
